5dae0b40a2f6213c5769826b5c05368a7dae3186
Doppel_R <doppelr1001@gmail.com>
2026-06-12 06:55:48 +0200

    data generator setup

---
+++ src/main/java/dev/kalwantspizza/perfectprocessing/datagen/DataGenerators.java
@@ -0,0 +1,20 @@
+package dev.kalwantspizza.perfectprocessing.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+
+import java.util.concurrent.CompletableFuture;
+
+public class DataGenerators {
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider));
+    }
+}
